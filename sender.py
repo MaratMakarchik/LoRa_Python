d65b7774d958efd778b44fe6c2319279dce5e99e
Traxmania <75.macar.macarob.dentio@gmail.com>
2025-07-23 17:39:35 +0300

    full chanel .py <--> .c<--> LoRa

--- sender.py
+++ sender.py
@@ -155,7 +155,8 @@ if __name__ == "__main__":
             message = controller.get_message() #проверка и получение сообщения
             
             if message:
-                print(f"MAIN LOOP | Received data: {message} | {time.strftime("%H:%M:%S", time.localtime())}")
+                time_struct = time.localtime()
+                print(f"MAIN LOOP | Received data: {message} | {time.strftime("%H:%M:%S", time_struct )}")
 
             controller.send_command(b'Hello') #отправка сообщения 
 
