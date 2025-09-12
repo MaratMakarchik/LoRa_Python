ca2a6d993ee6ce992a3c78f2793df049bcffed23
Makarov Makar <75.makar.makarov@gmail.com>
2025-09-12 11:34:01 +0300

    fixproblem fix 1.4

--- python/bdrv/config.py
+++ python/bdrv/config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 load_dotenv()
-SURVEY_TIME = os.getenv('SURVEY_TIME')
-BEACON_TIME = os.getenv('BEACON_TIME')
+SURVEY_TIME = int(os.getenv('SURVEY_TIME'))
+BEACON_TIME = int(os.getenv('BEACON_TIME'))
 CONFIG_SENSOR = os.getenv('CONFIG_SENSOR')
 ERROR_MESSAGE_LOG = os.getenv('ERROR_MESSAGE_LOG')
